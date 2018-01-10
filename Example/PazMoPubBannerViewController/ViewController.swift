862efa017f1c804a06bb02bb90664c73a3f639ba
Hilton Pintor <hiltonpintor@gmail.com>
2018-01-10 11:49:34 -0300

    Testing that code from pod can be used on example

--- Example/PazMoPubBannerViewController/ViewController.swift
+++ Example/PazMoPubBannerViewController/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import PazMoPubBannerViewController
 
-class ViewController: UIViewController {
+class ViewController: PazMoPubBannerViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(UpdateNotification.BannerViewActionWillPresent.name)
     }
 
     override func didReceiveMemoryWarning() {
